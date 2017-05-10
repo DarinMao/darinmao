f0f5a62510ca78f527ac7f4d2b8574f0ed0b5071
DarinMao <maodarin@gmail.com>
2017-05-09 20:02:54 -0400

    First commit

---
+++ index.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html lang="en">
+ <head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Darin Mao</title>
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link href="/css/1-col-portfolio.css" rel="stylesheet">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+ </head>
+ <body>
+	<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <span class="navbar-brand">Darin Mao</span>
+            </div>
+        </div>
+    </nav>
+	<div class="container">
+
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">Darin Mao
+                    <small>Check out my things</small>
+                </h1>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-7">
+                <a href="#">
+                    <img class="img-responsive" src="/images/smsspammer.png" alt="SMS Spammer">
+                </a>
+            </div>
+            <div class="col-md-5">
+                <h3>SMS Spammer</h3>
+                <h4><span class="glyphicon glyphicon-time" aria-hidden="true"></span> Last updated 2017=05-09</h4>
+                <p>Send a text message....or 30.......</p>
+                <a class="btn btn-primary" href="http://sms.darinmao.tk">View Project <span class="glyphicon glyphicon-chevron-right"></span></a>
+            </div>
+        </div>
+
+        <hr>
+
+        <div class="row">
+            <div class="col-md-7">
+                <a href="#">
+                    <img class="img-responsive" src="http://placehold.it/700x300" alt="">
+                </a>
+            </div>
+            <div class="col-md-5">
+                <h3>Belemnites</h3>
+                <h4><span class="glyphicon glyphicon-time" aria-hidden="true"></span> Last updated 2017=05-09</h4>
+                <p>Belemnites are squid-like, bullet-shaped organisms of the extinct order Belemnitida, part of the Cephalopod class. </p>
+                <a class="btn btn-primary" href="http://belemnites.darinmao.tk">View Project <span class="glyphicon glyphicon-chevron-right"></span></a>
+            </div>
+        </div>
+
+        <hr>
+
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Be nice. Don't copy my stuff. It's mine, not yours.</p>
+                </div>
+            </div>
+        </footer>
+    </div>
+ </body>
+</html>
